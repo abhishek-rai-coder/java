d85a5b5eedcbac8a17366db26a94e9a8f8ef8434
abhishek-rai-coder <117734823+abhishek-rai-coder@users.noreply.github.com>
2022-11-08 17:33:05 +0530

    Add files via upload

---
+++ Bill.java
@@ -0,0 +1,21 @@
+
+class Bill{
+public static void main (String user_input[])
+{ int amount=Integer.pa;
+rseInt(user_input[0]);
+
+if(amount>=1000)
+{ amount=amount-200;
+}
+elseif(amount>=500);
+{ amount=amount-100;
+}
+elseif(amount>=100);
+{ amount=amount-50;
+}
+else
+{ Syestem.out.println("no discount");
+}
+System.out.println("Bill to pay after discount:"+amount);
+}
+}
